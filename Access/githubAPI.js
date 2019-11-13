0a0470bb6244727d0825a555d483ccd6bdb88dde
DBoylan1 <boylande@tcd.ie>
2019-11-13 22:33:11 +0000

    Enter user name and returns json file of user details

---
+++ Access/githubAPI.js
@@ -0,0 +1,29 @@
+function getGithubApiResponse(apiUrl){
+    //console.log(apiUrl);
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",apiUrl,false);
+    Httpreq.send(null);
+    x = JSON.parse(Httpreq.responseText);
+    //console.log(x);
+    return x;
+}
+
+function getUserDetails() {
+    var username  = document.getElementById("username_inp").value;
+    var apiUrl = "https://api.github.com/users/" + username;
+    var userDetails = getGithubApiResponse(apiUrl);
+    userDetails = JSON.stringify(userDetails);
+    document.getElementById("userjson").innerHTML = "<p>HTML URL : " + userDetails.html_url +
+        "<p>User Details : " + userDetails + "</p>"; 
+    var repos = getReposForUser(username);
+    d3.select("body").selectAll("svg").remove();
+    
+   for (var i = 0; i < repos.length; i++){
+        var obj = repos[i];
+        var repoName = obj["name"];
+        console.log("Repo Name : " + repoName);
+       // createContributionChart(username, repoName);
+    }
+}
+
+
