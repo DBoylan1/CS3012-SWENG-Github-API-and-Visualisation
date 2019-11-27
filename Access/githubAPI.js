6e098636e60bb438de95bc0dca1b7bfcca75c761
DBoylan <47145188+DBoylan1@users.noreply.github.com>
2019-11-27 13:03:14 +0000

    Updated user details format
    Updated printing of user details: Username, created_at , public repos and when github was updated/

--- Access/githubAPI.js
+++ Access/githubAPI.js
@@ -13,16 +13,20 @@ function getUserDetails() {
     var username  = document.getElementById("username_inp").value;
     var apiUrl = "https://api.github.com/users/" + username;
     var userDetails = getGithubApiResponse(apiUrl);
-    userDetails = JSON.stringify(userDetails, null, 2);
-    document.getElementById("userjson").innerHTML =  "<p>User Details : " + userDetails + "</p>"; 
+   //userDetails = JSON.stringify(userDetails, null, 2);
     var repos = getReposForUser(username);
-    repos = JSON.stringify(repos);
+    repos = JSON.stringify(repos, null, 2);
+    document.getElementById("userjson").innerHTML =  "<p>Username : " + userDetails.login + "</p>" + "<p>Created : " + userDetails.created_at + "</p>"
+    + "<p>URL : " + userDetails.url + "</p>" + "<p>Repos URL : " + userDetails.repos_url + "</p>"  + "<p>Number of Public Repos : " + userDetails.public_repos + "</p>"
+    + "<p>Updated : " + userDetails.updated_at + "</p>"; 
+    //var repos = getReposForUser(username);
+    //repos = JSON.stringify(repos);
   
     }
 
 
 function getReposForUser(username) {
-    var apiUrl = "https://api.github.com/users/" + username + "/repos";
+    var apiUrl = "https://api.github.com/users/" + username + "/repos/full_name";
     return getGithubApiResponse(apiUrl);
 }
 
