435570797638c99a5aa6fa719faf45e15251c6f2
DBoylan <47145188+DBoylan1@users.noreply.github.com>
2019-11-28 15:04:56 +0000

    Create bar chart
    Function to create bar chart of contributions made by users in each of the public repos of the username entered.

--- Visualisation/githubAPI.js
+++ Visualisation/githubAPI.js
@@ -1,4 +1,3 @@
-
 function getGithubApiResponse(apiUrl){
     //console.log(apiUrl);
     var Httpreq = new XMLHttpRequest(); // a new request
@@ -21,13 +20,87 @@ function getUserDetails() {
     + "<p>Updated : " + userDetails.updated_at + "</p>"; 
     //var repos = getReposForUser(username);
     //repos = JSON.stringify(repos);
+
+    var repos = getReposForUser(username);
+    d3.select("body").selectAll("svg").remove();
+    
+    for (var i = 0; i < repos.length; i++){
+        var obj = repos[i];
+        var repoName = obj["name"];
+        console.log("Repo Name : " + repoName);
+        createContributionChart(username, repoName);
+    }
   
     }
 
 
 function getReposForUser(username) {
-    var apiUrl = "https://api.github.com/users/" + username + "/repos/full_name";
+    var apiUrl = "https://api.github.com/users/" + username + "/repos";
     return getGithubApiResponse(apiUrl);
 }
+function getContributorsForRepo(username, repo) {
+    var apiUrl = "https://api.github.com/repos/" + username + "/" + repo + "/contributors";
+    return getGithubApiResponse(apiUrl);
 
+}
 
+function createContributionChart(username, repo) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+    d3.json("https://api.github.com/repos/" + username + "/" + repo + "/contributors", function(error, data) {
+        if (error) throw error;
+
+        // format the data
+        data.forEach(function(d) {
+            d.contributions = +d.contributions;
+        });
+
+        // Scale the range of the data in the domains
+        x.domain(data.map(function(d) { return d.login; }));
+        y.domain([0, d3.max(data, function(d) { return d.contributions; })]);
+
+        // append the rectangles for the bar chart
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.login); })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(d.contributions); })
+            .attr("height", function(d) { return height - y(d.contributions); });
+
+        // add the x Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // add the y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        svg.append("text")
+            .text("Repository : " + repo)
+            .textAlign("center");
+    });
+}
